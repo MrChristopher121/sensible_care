97c1a66f47f392ac09bf0049360b1bffa9ead131
MrChristopher121 <chris@monteagle.com.au>
2025-06-29 14:42:59 +1000

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sensible-care.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
